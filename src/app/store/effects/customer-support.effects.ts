f3200c0334f00b0d7bfcff159044050c0c60f206
Sam M <emberdesignltd@protonmail.com>
2021-08-31 15:28:05 +0100

    feat(NgRx): Add customer service effect

--- src/app/store/effects/customer-support.effects.ts
+++ src/app/store/effects/customer-support.effects.ts
@@ -1,13 +1,28 @@
 import { Injectable } from '@angular/core';
-import { Actions, createEffect } from '@ngrx/effects';
-
-
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { map, mergeMap } from 'rxjs/operators';
+import { CustomerSupportService } from 'src/app/shared/services/customer-support.service';
+import * as supportActions from '../actions/customer-support.actions';
 
 @Injectable()
 export class CustomerSupportEffects {
+  sendMessage$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(supportActions.sendingCustomerSupportMessage),
+      mergeMap((action) =>
+        this.customerSupportService
+          .sendMessage(action.data)
+          .pipe(
+            map((sent) =>
+              supportActions.sendMessageStatus({ isSentSuccess: sent })
+            )
+          )
+      )
+    );
+  });
 
-
-
-  constructor(private actions$: Actions) {}
-
+  constructor(
+    private readonly actions$: Actions,
+    private readonly customerSupportService: CustomerSupportService
+  ) {}
 }
--- src/app/store/reducers/customer-support.reducer.ts
+++ src/app/store/reducers/customer-support.reducer.ts
@@ -1,22 +1,62 @@
 import { createReducer, on } from '@ngrx/store';
-import { sendingCustomerSupportMessage } from './../actions/customer-support.actions';
+import * as supportActions from './../actions/customer-support.actions';
 
 export const customerSupportFeatureKey = 'customerSupport';
 
 export interface State {
   name: string;
+  isSentSuccess: boolean | null;
 }
 
 export const initialState: State = {
   name: null,
+  isSentSuccess: null,
 };
+
+// export class CustomerSupportReducer {
+//   constructor(
+//     private readonly customerSupportService: CustomerSupportService
+//   ) {}
+
+//   reducer = createReducer(
+//     initialState,
+//     on(supportActions.sendingCustomerSupportMessage, (state, action) => {
+//       return {
+//         ...state,
+//         name: action.data.name,
+//       };
+//     }),
+//     // @ts-ignore
+//     on(supportActions.sendingCustomerSupportMessage, (state, action) => {
+//       return this.customerSupportService
+//         .sendMessage(action.data)
+//         .pipe(
+//           map((sent) =>
+//             supportActions.sendMessageStatus({ isSentSuccess: sent })
+//           )
+//         );
+//     }),
+//     on(supportActions.sendMessageStatus, (state, action) => {
+//       return {
+//         ...state,
+//         isSentSuccess: action.isSentSuccess,
+//       };
+//     })
+//   );
+// }
 
 export const reducer = createReducer(
   initialState,
-  on(sendingCustomerSupportMessage, (state, action) => {
+  on(supportActions.sendingCustomerSupportMessage, (state, action) => {
     return {
       ...state,
       name: action.data.name,
+    };
+  }),
+  on(supportActions.sendMessageStatus, (state, action) => {
+    return {
+      ...state,
+      isSentSuccess: action.isSentSuccess,
     };
   })
 );
