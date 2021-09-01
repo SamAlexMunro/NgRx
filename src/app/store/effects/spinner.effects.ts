9747b8e2c3c6384fffab54dc69b88d484c1d9334
Sam M <emberdesignltd@protonmail.com>
2021-09-01 09:17:02 +0100

    feat(NgRx): Add spinner effects logic

--- src/app/store/effects/spinner.effects.ts
+++ src/app/store/effects/spinner.effects.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
-import { Actions } from '@ngrx/effects';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { tap } from 'rxjs/operators';
+import * as AuthActions from '../actions/auth.actions';
 
 // ng generate effect store/effects/spinner --module=app.module.ts --root=true --skipTests=true
 
 @Injectable()
 export class SpinnerEffects {
-  constructor(private actions$: Actions) {}
+  spinnerOn$ = createEffect(
+    () => {
+      return this.actions$.pipe(
+        ofType(AuthActions.loginPage, AuthActions.loginModal),
+        tap(() => this.spinner.show())
+      );
+    },
+    { dispatch: false }
+  );
+  spinnerOff$ = createEffect(
+    () => {
+      return this.actions$.pipe(
+        ofType(AuthActions.loginFailure, AuthActions.loginSuccess),
+        tap(() => {
+          // Timeout just used to show the spinner normally we'd drop this.
+          setTimeout(() => {
+            this.spinner.hide();
+          }, 1000);
+        })
+      );
+    },
+    { dispatch: false }
+  );
+  constructor(
+    private actions$: Actions,
+    private readonly spinner: NgxSpinnerService
+  ) {}
 }
