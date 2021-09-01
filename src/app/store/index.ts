6d98fb9bd109419933f6d0ac0e5b9cd1da9c4fcb
Sam M <emberdesignltd@protonmail.com>
2021-09-01 08:24:29 +0100

    feat(NgRx): Add Auth NgRx files with Feature CLI

--- src/app/store/index.ts
+++ src/app/store/index.ts
@@ -1,6 +1,7 @@
 import { ActionReducerMap, MetaReducer } from '@ngrx/store';
 import { environment } from '../../environments/environment';
 import * as fromCustomerSupport from './reducers/customer-support.reducer';
+import * as fromAuth from './reducers/auth.reducer';
 
 /**
  * NgRx Schematics for generating store and templating
@@ -13,10 +14,12 @@ import * as fromCustomerSupport from './reducers/customer-support.reducer';
 
 export interface AppState {
   [fromCustomerSupport.customerSupportFeatureKey]: fromCustomerSupport.State;
+  [fromAuth.authFeatureKey]: fromAuth.State;
 }
 
 export const reducers: ActionReducerMap<AppState> = {
   [fromCustomerSupport.customerSupportFeatureKey]: fromCustomerSupport.reducer,
+  [fromAuth.authFeatureKey]: fromAuth.reducer,
 };
 
 export const metaReducers: MetaReducer<AppState>[] = !environment.production
